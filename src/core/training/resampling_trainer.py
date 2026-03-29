35b5bedd73b8d1ec42356c40c6f6044363c17d43
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-03-29 14:25:58 +0100

    Draft up Resampling trainer

--- src/core/training/resampling_trainer.py
+++ src/core/training/resampling_trainer.py
@@ -1,4 +1,8 @@
+from pathlib import Path
 from typing import override
+
+from transformers import TrainerCallback, TrainerControl, TrainerState, TrainingArguments
+from transformers.modeling_utils import PreTrainedModel
 
 from core.complexity_estimation.complexity_estimation_runner import (
     BaseComplexityEstimator,
@@ -11,27 +15,59 @@ from core.training.lora_trainer import LoRATrainer, LoRATrainerConfig
 from core.utils.logger import logger
 
 
-class ResamplingTrainerConfig(LoRATrainerConfig):
-    complexity_evaluation_dataset: QADatasetAdapter
-    complexity_estimator: BaseComplexityEstimator
+class EstimateComplexityCallback(TrainerCallback):
+    def __init__(
+        self,
+        complexity_evaluation_dataset: QADatasetAdapter,
+        complexity_estimator: BaseComplexityEstimator,
+        complexity_estimation_runner_generation_config: ModelGenerateConfig,
+        out_path: Path,
+    ) -> None:
+        super().__init__()
 
+        self._complexity_evaluation_dataset = complexity_evaluation_dataset
+        self._complexity_estimator = complexity_estimator
+        self._complexity_estimation_runner_generation_config = complexity_estimation_runner_generation_config
+        self._out_path = out_path
 
-class ResamplingTrainer(LoRATrainer[ResamplingTrainerConfig]):
-    def _estimate_complexity_for_epoch(self, epoch: int):
-        logger.info(f"Estimating complexity for epoch {epoch + 1}...")
+    @override
+    def on_epoch_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs) -> None:
+        logger.info(f"Estimating complexity for epoch {state.epoch}...")
+
+        model: PreTrainedModel = kwargs["model"]
 
         ComplexityEstimationRunner(
             config=ComplexityEstimationRunnerConfig(
-                out_path=self._path_for_epoch(epoch).as_posix(),
+                out_path=self._out_path.as_posix(),
                 answer_field_name="estimation_phase_answer",
                 answer_correctness_field_name="estimation_phase_answer_correctness",
-                generate_config=ModelGenerateConfig(max_new_tokens=1),
+                generate_config=self._complexity_estimation_runner_generation_config,
             ),
-            complexity_estimator=self.config.complexity_estimator,
-        ).estimate(dataset_adapter=self.config.complexity_evaluation_dataset, model=self._trainer.model)
+            complexity_estimator=self._complexity_estimator,
+        ).estimate(dataset_adapter=self._complexity_evaluation_dataset, model=model)
 
+
+class ResamplingTrainerConfig(LoRATrainerConfig):
+    complexity_evaluation_dataset: QADatasetAdapter
+    complexity_estimator: BaseComplexityEstimator
+    complexity_estimation_runner_generation_config: ModelGenerateConfig
+
+
+class ResamplingTrainer(LoRATrainer[ResamplingTrainerConfig]):
     @override
     def _prepare_data(self): ...
 
     @override
-    def _build_trainer(self, train_ds): ...
+    def _build_trainer(self, train_ds):
+        trainer = super()._build_trainer(train_ds)
+
+        trainer.add_callback(
+            EstimateComplexityCallback(
+                complexity_evaluation_dataset=self.config.complexity_evaluation_dataset,
+                complexity_estimator=self.config.complexity_estimator,
+                complexity_estimation_runner_generation_config=self.config.complexity_estimation_runner_generation_config,
+                out_path=Path(__file__).parent.joinpath("TODO"),
+            )
+        )
+
+        return trainer
