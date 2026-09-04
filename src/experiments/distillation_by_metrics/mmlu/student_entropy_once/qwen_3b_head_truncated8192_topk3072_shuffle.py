bdb543c8f4321e6d2d98c3eeaff4ef57018305d5
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-09-04 12:08:22 +0300

    Add exps

---
+++ src/experiments/distillation_by_metrics/mmlu/student_entropy_once/qwen_3b_head_truncated8192_topk3072_shuffle.py
@@ -0,0 +1,26 @@
+from core.dataset_samplers.base_sampler import BaseDatasetSamplerConfig
+from core.dataset_samplers.student_entropy_sampler import StudentEntropySampler
+from core.datasets.causal_dataset_adapter import CausalDatasetAdapter
+from core.datasets.mmlu.mmlu_reasoning_response_dataset import MMLUReasoningResponseDataset
+from core.datasets.mmlu.mmlu_single_token_response_dataset import QADatasetConfig
+from experiments.distillation_by_metrics.mmlu.shared import run
+
+run(
+    model_name="qwen_3b",
+    relative_out_path="./student_entropy_once/qwen_3b_head_truncated8192_topk3072_shuffle",
+    train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
+    train_dataset_adapter=CausalDatasetAdapter(
+        dataset=MMLUReasoningResponseDataset(
+            config=QADatasetConfig(
+                path="should be overridden by SetResamplingPathCallback",
+                dataset_id="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
+            ),
+            # Will be overridden
+            tokenizer=None,  # type: ignore
+        ),
+        dataset_sampler=StudentEntropySampler(BaseDatasetSamplerConfig(top_k=3072)),
+    ),
+    save_schedule=[5, 10, 25, 40, 50],
+    resampling_schedule=[0],
+    shuffle=True,
+)
---
+++ src/experiments/distillation_by_metrics/mmlu/student_entropy_proportional/phi4_mini_head_truncated8192_seed44.py
@@ -0,0 +1,19 @@
+from core.complexity_estimation.entropy.single_token_entropy_with_random_estimator import (
+    SingleTokenEntropyWithRandomEstimator,
+)
+from core.dataset_samplers.student_entropy_proportional_sampler import StudentEntropyProportionalSampler
+from experiments.distillation_by_metrics.mmlu.shared import (
+    get_merged_adapter_with_data_mix,
+    run,
+)
+
+run(
+    model_name="phi4_mini",
+    relative_out_path="./student_entropy_proportional/phi4_mini_head_truncated8192_shuffle",
+    train_dataset="train_corrected_answer_deepseek_v4_pro_and_others_head_truncated8192",
+    train_dataset_adapter=get_merged_adapter_with_data_mix(StudentEntropyProportionalSampler),
+    save_schedule=[20, 50, 100, 150, 200],
+    complexity_estimator_override=SingleTokenEntropyWithRandomEstimator(),
+    shuffle=True,
+    seed=44,
+)
